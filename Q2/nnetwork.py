823a046e2e1d458ea743090d6bd982027bcaa10e
arpanmangal <mangalarpan@gmail.com>
2019-04-01 21:54:24 +0530

    improved something

--- Q2/nnetwork.py
+++ Q2/nnetwork.py
@@ -2,6 +2,7 @@
 The class of Neural Network
 """
 import numpy as np
+from plot import make_confusion_matrix
 
 class NNetwork:
     def __init__ (self, num_input, sizes, num_output, batch_size):
@@ -19,17 +20,17 @@ class NNetwork:
         #     self.weights[l] = np.random.random((sizes[l], sizes[l-1]))
         #     self.biases[l] = np.random.random((sizes[l],))
 
-        self.biases = [np.random.randn(currSize, ) for currSize in sizes[1:]]
+        self.biases = [np.random.randn(currSize, 1) for currSize in sizes[1:]]
         self.weights = [np.random.randn(currSize, prevSize) for currSize, prevSize in zip(sizes[1:], sizes[:-1])]
             
         # self.biases[0] = np.array((0, 0))
         # self.weights[0] = np.array((0, 0))
         # self.biases = np.array(self.biases)
         # self.weights = np.array(self.weights)
-        for b in self.biases:
-            print (b.shape)
-        for w in self.weights:
-            print (w.shape)
+        # for b in self.biases:
+        #     print (b)
+        # for w in self.weights:
+        #     print (w)
 
 
     def predict (self, X):
@@ -48,6 +49,11 @@ class NNetwork:
 
 
     def train (self, X, Y, epochs, eta):
+        # Y = Y.reshape(len(Y), 1)
+        # print (Y.shape)
+        # print (Y)
+        # print (X.shape)
+        # print (X[0].shape)
         trainData = np.array(list(zip(X, Y)))
         print (trainData.shape)
         for e in range(epochs):
@@ -62,6 +68,12 @@ class NNetwork:
 
             accuracy = self.evaluate (X, Y)
             print ("Epoch: %d | Accuracy: %.2f" % (e, 100 * accuracy))
+            # print (self.biases)
+            # print (self.weights)
+
+        X, Y = zip(*trainData)
+        predictions = self.predict (X)
+        make_confusion_matrix (Y, predictions)
 
 
     def MBGD (self, mini_batch, eta):
@@ -69,38 +81,49 @@ class NNetwork:
         Mini-Batch Gradient Descent
         Update the weights and biases for this mini_batch
         """
-        X, Y = zip(*mini_batch)
-        del_weights = [np.zeros(w.shape) for w in self.weights]
+        # X, Y = zip(*mini_batch)
         del_biases = [np.zeros(b.shape) for b in self.biases]
+        del_weights = [np.zeros(w.shape) for w in self.weights]
 
-        for x, y in zip(X, Y[1:]):
+        for x, y in mini_batch:
             # Do a feedforward and compute z and delta for each layer
+            x = x.reshape(len(x), 1)
             zs = [] # Set of z's
             azs = [] # Set of a's
-            deltas = [None] * self.L
             a = x
             azs.append(a)
             for b, w in zip(self.biases, self.weights):
+                # print (w.shape, a.shape, b.shape)
                 z = np.matmul(w, a) + b
+                print (z.shape, w.shape, a.shape, b.shape)
                 zs.append(z)
                 a = self.sigmoid(z)
                 azs.append (a)
+                # print (b.shape, z.shape, a.shape)
+
+            print (zs)
+            exit(0)
 
             # Do a backpropogate
             gradient_C = (a - y)
-            deltas[-1] = np.multiply (gradient_C, self.sigmoid_prime(zs[-1]))
-            del_biases[-1] += deltas[-1]
-            del_weights[-1] += np.matmul (deltas[-1].reshape(len(deltas[-1]), 1), azs[-2].reshape(1, len(azs[-2])))
+            delta = np.multiply (gradient_C, self.sigmoid_prime(zs[-1]))
+            del_biases[-1] += delta
+            del_weights[-1] += np.matmul (delta.reshape(len(delta), 1), azs[-2].reshape(1, len(azs[-2])))
+
+            for l in range (2, self.L + 1):
+                sp = self.sigmoid_prime(z[-1])
+                delta = np.matmul (self.weights[-l + 1].transpose(), delta) * sp
+                del_biases[-l] += delta
+                del_weights[-l] += np.matmul (delta.reshape(len(delta), 1), azs[-l-1].reshape(1, len(azs[-l-1])))
+
+            # for l in range (self.L - 2, -1, -1):
+            #     delta = np.multiply( (np.matmul(np.transpose(self.weights[l+1]), deltas[l+1])), self.sigmoid_prime(z[l]))
+            #     del_biases[l] += delta
+            #     del_weights[l] += np.matmul (delta.reshape(len(delta), 1), azs[l].reshape(1, len(azs[l])))
 
 
-            for l in range (self.L - 2, -1, -1):
-                deltas[l] = np.multiply( (np.matmul(np.transpose(self.weights[l+1]), deltas[l+1])), self.sigmoid_prime(z[l]))
-                del_biases[l] += deltas[l]
-                del_weights[l] += np.matmul (deltas[l].reshape(len(deltas[l]), 1), azs[l].reshape(1, len(azs[l])))
-
-
-        del_biases = [db / len(X) for db in del_biases]
-        del_weights = [dw / len(X) for dw in del_weights]
+        del_biases = [db / len(mini_batch) for db in del_biases]
+        del_weights = [dw / len(mini_batch) for dw in del_weights]
 
         self.biases = [b - eta * db for b, db in zip(self.biases, del_biases)]
         self.weights = [w - eta * dw for w, dw in zip(self.weights, del_weights)]
@@ -117,7 +140,7 @@ class NNetwork:
 
     # Helper Functions
     def sigmoid (self, z):
-        return 1 / (1 + np.exp(-z))
+        return 1.0 / (1.0 + np.exp(-z))
 
     def sigmoid_prime (self, z):
         sig = self.sigmoid (z)
